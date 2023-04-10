83d0b4e81eb5d8cd8d1dd7e225393730d9b95019
tuan220199 <phamtuan.220199@gamil.com>
2023-04-10 15:50:20 +0200

    Upload files

--- pet_project/pet/car/apps.py
+++ pet_project/pet/car/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CarRecommendConfig(AppConfig):
+class CarConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'car_recommend'
+    name = 'car'
