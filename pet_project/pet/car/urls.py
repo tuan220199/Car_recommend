36093c1e7a3305737bc250e60b918ebe9429b749
tuan220199 <phamtuan.220199@gamil.com>
2023-04-18 01:06:12 +0200

    Finish the categories feature + Put more security for log in with normal account and super user acconut + working on create new car post

--- pet_project/pet/car/urls.py
+++ pet_project/pet/car/urls.py
@@ -6,11 +6,12 @@ urlpatterns = [
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
-
     path("categories", views.categories, name="categories"),
-    
+    path("category/<int:category_id>", views.category_each_brand, name="category_each_brand"),
+    path("new_post", views.create_new_car_post, name="create_new_car_post"),
+
     #API path
-    path("api/category/<int:category_id>/", views.category_each_brand_api, name="category_each_brand_api"),
+    path("category/api/<int:category_id>/", views.category_each_brand_api, name="category_each_brand_api"),
     path("api/cars/", views.cars_api, name="cars"),
     path("api/pagnigation/", views.pagnigation_api, name="pagnigation"),
     path("api/categories/", views.categories_api, name="categories_api")
--- pet_project/pet/car/views.py
+++ pet_project/pet/car/views.py
@@ -9,7 +9,11 @@ from django.views.decorators.csrf import csrf_exempt
 from django.core.paginator import Paginator
 from .models import *
 import math
+from django.contrib.auth.decorators import user_passes_test
 
+# Function is_superuser to check if the account is super user or not
+def is_superuser(user):
+    return user.is_superuser
 
 def index(request):
     return render(request, "car/index.html")
@@ -17,33 +21,86 @@ def index(request):
 def categories(request):
     return render(request, "car/categories.html")
 
+@login_required
+def create_new_car_post(request):
+    if request.method == "POST":
+        
+        # current user
+        current_user = request.user
+
+        # Retrieve dat from form
+        price = float(request.POST["price"])
+        model = request.POST["model"]
+        year = float(request.POST["year"])
+        mileage = float(request.POST["mileage"])
+        engine_capacity = float(request.POST["engine_capacity"])
+        transmission = request.POST["transmission"]
+        drive = request.POST["drive"]
+        hand_drive = request.POST["hand_drive"]
+        fuel = request.POST["fuel"]
+        image_url = request.POST["image_url"]
+        
+        # Convert mark category from string into object in database
+        mark_category_input = request.POST["mark_category"]
+        categories = Category.objects.all()
+        category_id = 0
+        for category in categories:
+            if mark_category_input == category.groups:
+                category_id = category.id
+
+        mark_category = Category.objects.get(pk=category_id)
+
+        # Create a new car post object and save into database 
+        new_car = Car(price=price, year=year, mileage=mileage, engine_capacity=engine_capacity, model=model, transmission=transmission, drive=drive, hand_drive=hand_drive, fuel=fuel, image_url=image_url, mark_category=mark_category, owner=current_user) 
+        new_car.save()
+        
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "car/create_new_post.html")
+
+
+
+def category_each_brand(request, category_id):
+    category_id = category_id
+    return render(request, "car/category.html",{
+        "category_id": category_id
+    })
+
+
 def category_each_brand_api(request, category_id):
-    category = Category.objects.get(pk=category_id)
-    cars = Car.objects.order_by("-id").filter(mark_category=category)
+    try:
+        category = Category.objects.get(pk=category_id)
+        cars = Car.objects.order_by("-id").filter(mark_category=category)
 
-    #Pagination 
-    paginator = Paginator(cars, 10) # Show 10 posts per page.
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
+        #Pagination 
+        paginator = Paginator(cars, 10) # Show 10 posts per page.
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
 
-    car_list = []
-    for car in page_obj:
-        car_list.append(car.serialize())
+        car_list = []
+        for car in page_obj:
+            car_list.append(car.serialize())
 
-    data = {
-        'results': car_list,
-        'count': math.ceil(paginator.count/10),
-        'num_pages': page_number,
-    }
+        data = {
+            'results': car_list,
+            'count': math.ceil(paginator.count/10),
+            'num_pages': page_number,
+        }
 
-    return JsonResponse(data)
+        return JsonResponse(data)
 
+    except Exception as e:
+        # Log the error to the console for debugging purposes.
+        print(e)
+        return JsonResponse({'error': 'Something went wrong.'})
 
+# This view "categories_api" only superuser account can log in
+#@user_passes_test(is_superuser)
 def categories_api(request):
     categories = Category.objects.all()
     return JsonResponse([category.serialize() for category in categories], safe=False)
 
-
+#@user_passes_test(is_superuser)
 def pagnigation_api(request):
     """
     This function paginates a list of cars and returns a JSON response with the paginated data.
@@ -71,7 +128,8 @@ def pagnigation_api(request):
 
     return JsonResponse(data)
 
-
+# This view "cars_api" can only be accessed after the user log in
+@login_required
 def cars_api(request):
     """
     This function retrieves all Car objects from the database and returns them as a JSON response.
